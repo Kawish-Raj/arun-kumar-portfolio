833627be1b2f419e893b93ad077a90fa6adb380e
Kawish-Raj <rajkawish@gmail.com>
2025-05-06 10:14:18 +1000

    fixed sidebar to the top left

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import AboutMe from './components/AboutMe'
 import Qualifications from './components/Qualifications'
 import Work from './components/Work'
 import Footer from './components/Footer'
+import SideBar from './components/SideBar'
 
 function App() {
 
   return (
     <div className = "app-container">
+      <SideBar />
       <HeroArea />
       <AboutMe />
       <Qualifications />
