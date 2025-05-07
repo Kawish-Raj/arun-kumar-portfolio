29b167fc975d90a78bb8b72519b93c0fcacebb0b
Kawish-Raj <rajkawish@gmail.com>
2025-05-07 22:47:49 +1000

    made somewhat of a fill up transition

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -7,13 +7,13 @@ export default function SideBar({isMainContent}){
             <ul>
                 <li className={isMainContent === "about me" ? 
                     `${styles.fadeIn}`: ''}><a href='#about-id'>About Me</a>
-                    <div className={styles.underline}></div></li>
+                    <div className={`${styles.underline} ${isMainContent === "about me" ? styles.fillUp : ''}`}></div></li>
                 <li className={isMainContent === "qualifications" ?
                      `${styles.fadeIn}`: ''}><a href='#qualifications-id'>Qualifications</a>
-                     <div className={styles.underline}></div></li>
+                     <div className={`${styles.underline} ${isMainContent === "qualifications" ? styles.fillUp : ''}`}></div></li>
                 <li className={isMainContent === "work" ? 
                     `${styles.fadeIn}`: ''}><a href='#work-id'>Work and Experiences</a>
-                    <div className={styles.underline}></div></li>
+                    <div className={`${styles.underline} ${isMainContent=== "work" ? styles.fillUp : ''}`}></div></li>
             </ul>
         </div>
     )
