3312972ee064c93780db76abf7e1fa018b2f99ce
Kawish-Raj <rajkawish@gmail.com>
2025-05-07 19:12:21 +1000

    made the underline div for the side bar menus

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -6,11 +6,14 @@ export default function SideBar({isMainContent}){
             <h3>AK</h3>
             <ul>
                 <li className={isMainContent === "about me" ? 
-                    `${styles.fadeIn}`: ''}><a href='#about-id'>About Me</a></li>
+                    `${styles.fadeIn}`: ''}><a href='#about-id'>About Me</a>
+                    <div className={styles.underline}></div></li>
                 <li className={isMainContent === "qualifications" ?
-                     `${styles.fadeIn}`: ''}><a href='#qualifications-id'>Qualifications</a></li>
+                     `${styles.fadeIn}`: ''}><a href='#qualifications-id'>Qualifications</a>
+                     <div className={styles.underline}></div></li>
                 <li className={isMainContent === "work" ? 
-                    `${styles.fadeIn}`: ''}><a href='#work-id'>Work and Experiences</a></li>
+                    `${styles.fadeIn}`: ''}><a href='#work-id'>Work and Experiences</a>
+                    <div className={styles.underline}></div></li>
             </ul>
         </div>
     )
