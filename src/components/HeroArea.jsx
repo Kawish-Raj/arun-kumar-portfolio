33c64d726fdac6fd8834a398f9ad1d331f6e4c8e
Kawish-Raj <rajkawish@gmail.com>
2025-05-09 18:15:35 +1000

    chose a better color scheme and font style

--- src/components/HeroArea.jsx
+++ src/components/HeroArea.jsx
@@ -1,9 +1,14 @@
 import styles from '../css-components/HeroArea.module.css';
 
-export default function HeroArea(){
-    return(
+export default function HeroArea() {
+    return (
         <div className={`global-HeroArea homeComponent ${styles.heroArea}`}>
-            {/* <h1>Hero Area</h1> */}
+            <div className={`${styles.messageContainer}`}>
+                <h1 className={styles.hi}>hi!</h1>
+                <h1>i'm</h1>
+                <h1><span className={styles.firstName}>Arun</span>
+                <span className={styles.secondName}>Kumar</span></h1>
+            </div>
         </div>
     )
 }
