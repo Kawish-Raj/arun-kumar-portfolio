895163fe6d64aaffb96396a2465c9f6d39db32e8
Kawish-Raj <rajkawish@gmail.com>
2025-05-10 16:01:50 +1000

    fixed the side-bar desgin for the mission bettina

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -3,13 +3,15 @@ import styles from '../css-components/SideBar.module.css';
 export default function SideBar({ isMainContent }) {
     return (
         <div className={`global-SideBar homeComponent ${styles.sideBar}`}>
-            <h3>AK</h3>
+            <div className={styles.sideBarContentContainer}>
+            <h3 className={styles.logoSideBar}>AK</h3>
             <a className={isMainContent === "about me" ?
                 `${styles.fadeIn}` : ''} href='#about-id'>About Me</a>
             <a className={isMainContent === "qualifications" ?
                 `${styles.fadeIn}` : ''} href='#qualifications-id'>Qualifications</a>
             <a className={isMainContent === "work" ?
                 `${styles.fadeIn}` : ''} href='#work-id'>My Work</a>
+            </div>
         </div>
     )
 }
