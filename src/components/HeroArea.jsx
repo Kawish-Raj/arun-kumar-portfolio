5d3426be2456fdd60009a1fd7e3706e19bfc69b1
Kawish-Raj <rajkawish@gmail.com>
2025-05-10 15:25:01 +1000

    some hero area changes

--- src/components/HeroArea.jsx
+++ src/components/HeroArea.jsx
@@ -1,14 +1,16 @@
 import styles from '../css-components/HeroArea.module.css';
+import demoPerson from '../assets/demo-person.png'
 
 export default function HeroArea() {
     return (
         <div className={`global-HeroArea homeComponent ${styles.heroArea}`}>
-            <div className={`${styles.messageContainer}`}>
+            {/* <div className={`${styles.introGridContainer}`}>
+                <div className={styles.empty}></div>
                 <h1 className={styles.hi}>hi!</h1>
-                <h1>i'm</h1>
-                <h1><span className={styles.firstName}>Arun</span>
-                <span className={styles.secondName}>Kumar</span></h1>
-            </div>
+                <h1 className={styles.im}>i'm</h1>
+                <h1 className={styles.firstName}>Arun</h1>
+                <h1 className={styles.secondName}>Kumar</h1>
+            </div> */}
         </div>
     )
 }
