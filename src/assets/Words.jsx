1281ca0cd7d6338234cbbcf907a6d3502cf0ca52
Kawish-Raj <rajkawish@gmail.com>
2025-05-10 21:21:52 +1000

    affect for all the tag line words finished need to extra words at the bottom

---
+++ src/assets/Words.jsx
@@ -0,0 +1,8 @@
+const words = [
+    ['B', 'u', 'i', 'l', 'd', 'i', 'n', 'g'],
+    ['T', 'o', 'm', 'o', 'r', 'r', 'o', 'w', "'", 's'],
+    ['S', 'c', 'h', 'o', 'o', 'l', 's'],
+    ['T', 'o', 'd', 'a', 'y']
+]
+
+export default words;
--- src/components/HeroArea.jsx
+++ src/components/HeroArea.jsx
@@ -1,62 +1,51 @@
 import styles from '../css-components/HeroArea.module.css';
 import { useEffect, useRef, createRef } from 'react';
+import words from '../assets/Words';
 
 export default function HeroArea() {
-  const speeds = [0.3, -0.2, 0.7, -1.3, 0.5]; // can randomize or customize more
-  const word1 = ['B', 'u', 'i', 'l', 'd', 'i', 'n', 'g'];
-  const word2 = ['T', 'o', 'm', 'o', 'r', 'r', 'o', 'w', "'", 's']
-  const letters = [...word1, ...word2]
-  const rotationSpeeds = [0.01, 0.03, -0.02, 0.009, -0.01 ];
-  
-  // Create a list of refs dynamically for each letter
-  const refs = useRef(letters.map(() => createRef()));
+    const speeds = [0.3, -0.2, 0.7, -1.3, 0.5]; // can randomize or customize more
+    const letters = [...words[0], ...words[1], ...words[2], ...words[3]]
+    const rotationSpeeds = [0.01, 0.03, -0.02, 0.009, -0.01];
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollY = window.scrollY;
-      const viewportHeight = window.innerHeight;
-  
-      if (scrollY < viewportHeight) {
-        refs.current.forEach((ref, idx) => {
-          const speed = speeds[idx % speeds.length];
-          const rotationSpeed = rotationSpeeds[idx % rotationSpeeds.length];
-          if (ref.current) {
-            ref.current.style.transform = `translateY(${scrollY * speed}px) rotate(${scrollY * rotationSpeed}deg)`;
-          }
-        });
-      }
-    };
-  
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
+    // Create a list of refs dynamically for each letter
+    const refs = useRef(letters.map(() => createRef()));
 
-  return (
-    <div className={`global-HeroArea homeComponent ${styles.heroArea}`}>
-      <div className={styles.tagLineContainer}>
-        <div className={styles.wordContainer}>
-        {word1.map((letter, idx) => (
-          <div
-            key={idx}
-            ref={refs.current[idx]}>{letter}</div>
-        ))}
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollY = window.scrollY;
+            const viewportHeight = window.innerHeight;
+
+            if (scrollY < (0.5 * viewportHeight)) {
+                refs.current.forEach((ref, idx) => {
+                    const speed = speeds[idx % speeds.length];
+                    const rotationSpeed = rotationSpeeds[idx % rotationSpeeds.length];
+                    if (ref.current) {
+                        ref.current.style.transform = `translateY(${scrollY * speed}px) rotate(${scrollY * rotationSpeed}deg)`;
+                    }
+                });
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
+    return (
+        <div className={`global-HeroArea homeComponent ${styles.heroArea}`}>
+            <div className={styles.tagLineContainer}>
+                {words.map((word, wordIdx) => (
+                    <div key={wordIdx} className={styles.wordContainer}>
+                        {word.map((letter, idx) => {
+                            const flatIndex = words.slice(0, wordIdx).reduce((sum, w) => sum + w.length, 0) + idx;
+                            return (
+                                <div key={flatIndex} ref={refs.current[flatIndex]}>
+                                    {letter}
+                                </div>
+                            );
+                        })}
+                    </div>
+                ))}
+            </div>
         </div>
-        <div className={styles.wordContainer}>
-        {word2.map((letter, idx) => (
-          <div
-            key={idx + (word1.length - 1)}
-            ref={refs.current[idx + (word1.length - 1)]}>{letter}</div>
-        ))}
-        </div>
-        
-
-
-
-        {/* <div>ing</div> */}
-        {/* <div> Tomorrow's</div> */}
-        <div> Schools</div>
-        <div> Today</div>
-      </div>
-    </div>
-  );
+    );
 }
