96d2f2b2a8fdd235c8eda279edb512234d34de91
Kawish-Raj <rajkawish@gmail.com>
2025-05-12 17:13:44 +1000

    made the content part of the about me need to adjust the font and color and style

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -5,7 +5,7 @@ import useSideBarEnterTrigger from '../customhooks/useSideBarEnterTrigger';
 
 export default function AboutMe({ setIsMainContent, setSideBarMode }) {
     const divRef1 = useTopDistanceTrigger(setIsMainContent, 0);
-    const divRef2 = useSideBarEnterTrigger(setSideBarMode);
+    const divRef2 = useSideBarEnterTrigger(setSideBarMode, 60);
     
     const setRefs = (el) => {
         divRef1.current = el;
@@ -18,6 +18,23 @@ export default function AboutMe({ setIsMainContent, setSideBarMode }) {
                 <div className={styles.headingWord}>hi.</div>
                 <div className={styles.headingWord}>i'm</div>
                 <div className={styles.headingWord}>arun kumar.</div>
+            </div>
+            <div className={styles.aboutmePara}> 
+                <p>I began my journey in education as a simple school teacher,
+                     and over the years, I have remained deeply committed to this
+                      field. I have always believed that education is one of the 
+                      most powerful tools for positive change, not just for 
+                      individuals, but for society as a whole.
+                </p>
+                <p>
+                    Throughout my career, I’ve been fortunate to be involved in 
+                    establishing and managing educational institutions across 
+                    Bihar and Jharkhand. My focus has consistently been on 
+                    improving the quality of education at both school and college 
+                    levels. I try to personally stay involved in the functioning of 
+                    these institutions to ensure that students and teachers have the 
+                    right environment to grow.
+                </p>
             </div>
             </div>
         </div>
