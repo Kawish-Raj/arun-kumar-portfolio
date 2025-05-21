3d82762a60e8cc271f554ec0507221c57afb0c31
Kawish-Raj <rajkawish@gmail.com>
2025-05-21 11:43:37 +1000

    adjusted the qualifications heading and timeline container spacing

--- src/components/Qualifications.jsx
+++ src/components/Qualifications.jsx
@@ -1,10 +1,15 @@
 import styles from '../css-components/Qualifications.module.css';
 import useTopDistanceTrigger from '../customhooks/useTopDistanceTrigger';
-export default function Qualifications({setIsMainContent}){
+export default function Qualifications({ setIsMainContent }) {
     const divRef = useTopDistanceTrigger(setIsMainContent, 1);
-    return(
-        <div ref={divRef} id="qualifications-id" className={`global-Qualifications homeComponent ${styles.qualifications}`}>
-            <h1>Qualifications</h1>
+    return (
+        <div ref={divRef} id="qualifications-id"
+            className={`global-Qualifications homeComponent 
+        ${styles.qualifications}`}>
+            <div className={styles.headContainer}>
+                <h1 className={styles.mainHeading}>Qualifications</h1>
+            </div>
+            <div className={styles.timelineContainer}></div>
         </div>
     )
 }
