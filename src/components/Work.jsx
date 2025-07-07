e6615ee5f58e98a0ac802e2406fd820e00327698
Kawish-Raj <rajkawish@gmail.com>
2025-07-07 12:13:03 +1000

    added some masking that actually works will explore further

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,11 +1,20 @@
 import styles from '../css-components/Work.module.css';
 import useTopDistanceTrigger from '../customhooks/useTopDistanceTrigger';
 
-export default function Work({setIsMainContent}){
+export default function Work({ setIsMainContent }) {
     const divRef = useTopDistanceTrigger(setIsMainContent, 2);
-    return(
-        <div ref={divRef} id="work-id" className={`global-Work homeComponent ${styles.work}`}>
-            <h1>Work and Experiences</h1>
+    return (
+        <div ref={divRef} id="work-id" className={`global-Work ${styles.work}`}>
+            {/* <h1>Work and Experiences</h1> */}
+            <div className={styles.stickyWorkContainer}>
+                <div className={styles.halfTransparent}>
+                    Your content here
+                </div>
+                <div className={styles.theOneBellow}>
+                    I don't know what to write here.
+                </div>
+            </div>
         </div>
+
     )
 }
