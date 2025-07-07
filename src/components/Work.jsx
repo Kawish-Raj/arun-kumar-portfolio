64b3004d7829bb4322a9231d681de72e6c1e38e5
Kawish-Raj <rajkawish@gmail.com>
2025-07-07 12:36:01 +1000

    scroll detection works when the experince components sticks just need to add the effect now

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,10 +1,41 @@
 import styles from '../css-components/Work.module.css';
 import useTopDistanceTrigger from '../customhooks/useTopDistanceTrigger';
+import { useRef,useEffect } from 'react';
 
 export default function Work({ setIsMainContent }) {
+    
+    const workComponentRef = useRef(null);
+
+    const setRefs = (el) => {
+        divRef.current = el;
+        workComponentRef.current =el;
+    }
+    
     const divRef = useTopDistanceTrigger(setIsMainContent, 2);
+
+    useEffect(() => {
+        const workComponent = workComponentRef.current;
+
+        function handleScroll() {
+            const scrollY = window.scrollY;
+            const offsetTop = workComponent.offsetTop;
+            const pinStart = offsetTop;
+            const pinEnd = offsetTop + workComponent.getBoundingClientRect().height;
+        
+            if((scrollY >= pinStart && scrollY <= pinEnd)){
+            console.log("scroll detection works inside the box");
+        }
+        }
+
+        window.addEventListener('scroll',handleScroll, { passive: false});
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        }
+    }, []);
+
     return (
-        <div ref={divRef} id="work-id" className={`global-Work ${styles.work}`}>
+        <div ref={setRefs} id="work-id" className={`global-Work ${styles.work}`}>
             {/* <h1>Work and Experiences</h1> */}
             <div className={styles.stickyWorkContainer}>
                 <div className={styles.halfTransparent}>
