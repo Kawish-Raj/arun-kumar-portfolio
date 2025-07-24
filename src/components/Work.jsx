6f96e857ce1a6cbd869ad016101a051aed70a8e9
Kawish-Raj <rajkawish@gmail.com>
2025-07-24 17:34:08 +1000

    was able to fit two images plus added more images to asset

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -4,6 +4,8 @@ import { useRef, useEffect } from 'react';
 import papaPlaying from "../assets/papa-images/papa-playing.jpg"
 import papaSitting from "../assets/papa-images/papa-sitting.jpg"
 import papaCutting from "../assets/papa-images/papa-cutting.jpg"
+import inspection from "../assets/papa-images/inspection.jpg"
+import schoolRoses from "../assets/papa-images/schoolroses.jpg"
 
 export default function Work({ setIsMainContent }) {
 
@@ -31,10 +33,6 @@ export default function Work({ setIsMainContent }) {
             const experienceLength = workComponent.getBoundingClientRect().height / noOfExperiences;
             const windowHeight = window.innerHeight;
             const residueLength = experienceLength - windowHeight;
-
-            console.log("experience length: ", experienceLength)
-            console.log("container length: ", workComponent.getBoundingClientRect().height);
-            console.log("window height", window.innerHeight);
 
             //BEFORE first heading
             //  scrollY < pinStart
@@ -113,19 +111,22 @@ export default function Work({ setIsMainContent }) {
                         <div className={styles.textContent}>
                             <h1 className={styles.expHeading}>Experience 1</h1>
                             <p className={styles.firstPara}>
-                        Just writing whatever I want to.
-                        I didn't want to use Lrem Impusm cause it's for
-                        losers. I don't get it why it's popular when you can 
-                        fill the paras with whatever you want. Just write anything and 
-                        no one will notice cause its filler content my mate. </p>
-                    <p className={styles.secondPara}>Just writing whatever I want to.
-                        I didn't want to use Lrem Impusm cause it's for
-                        losers. I don't get it why it's popular when you can 
-                        fill the paras with whatever you want. Just write anything and 
-                        no one will notice cause its filler content my mate.</p>
+                                Just writing whatever I want to.
+                                I didn't want to use Lrem Impusm cause it's for
+                                losers. I don't get it why it's popular when you can
+                                fill the paras with whatever you want. Just write anything and
+                                no one will notice cause its filler content my mate. 
+                            </p>
+                            <p className={styles.secondPara}>Just writing whatever I want to.
+                                I didn't want to use Lrem Impusm cause it's for
+                                losers. I don't get it why it's popular when you can
+                                fill the paras with whatever you want. Just write anything and
+                                no one will notice cause its filler content my mate.
+                            </p>
                         </div>
                         <div className={styles.imageContent}>
                             <img className={styles.expImage} src={papaCutting} alt='sir playing image'></img>
+                            <img className={`${styles.expImage} ${styles.expImageRightTilt}`} src={papaSitting} alt='sir sitting image'></img>
                         </div>
                     </div>
                 </div>
@@ -135,18 +136,19 @@ export default function Work({ setIsMainContent }) {
                         <div className={styles.textContent}>
                             <h1 className={styles.expHeading}>2nd Experience</h1>
                             <p className={styles.firstPara}>
-                        What was I going to write. A sentence. Two sentences. 
-                        Write this To the whole world. Still nah hypotheticaly scenario. 
-                        Bihar is the best state. Better than all of them. I am studying with 
-                        two idiots in RMIT building 8. </p>
-                    <p className={styles.secondPara}>Just writing whatever I want to.
-                        I didn't want to use Lrem Impusm cause it's for
-                        losers. I don't get it why it's popular when you can 
-                        fill the paras with whatever you want. Just write anything and 
-                        no one will notice cause its filler content my mate.</p>
+                                What was I going to write. A sentence. Two sentences.
+                                Write this To the whole world. Still nah hypotheticaly scenario.
+                                Bihar is the best state. Better than all of them. I am studying with
+                                two idiots in RMIT building 8. </p>
+                            <p className={styles.secondPara}>Just writing whatever I want to.
+                                I didn't want to use Lrem Impusm cause it's for
+                                losers. I don't get it why it's popular when you can
+                                fill the paras with whatever you want. Just write anything and
+                                no one will notice cause its filler content my mate.</p>
                         </div>
                         <div className={styles.imageContent}>
-                            <img className={styles.expImage} src={papaSitting} alt='sir playing image'></img>
+                            <img className={styles.expImage} src={papaPlaying} alt='sir playing image'></img>
+                            <img className={`${styles.expImageRightTilt} ${styles.expImage}`} src={inspection} alt='science fair inspection'></img>
                         </div>
                     </div>
                 </div>
